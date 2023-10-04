92fe29261be6458ef43fad1c3f9ad24a0e6057cc
Maxence Chevee <max.rothen@hotmail.fr>
2023-10-04 17:14:37 +0200

    Dark mode

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ if (window.location.pathname === "/second.html") {
         window.location.href = "/index.html"; // Remplacez "/" par l'URL de votre page principale
     }, 5000);
 }
+const toggle_btn = document.getElementById('theme-btn');
+
+const body = document.getElementsByTagName('body')[0];
+
+const dark_theme_class = 'dark';
+
+
+
+toggle_btn.addEventListener('click', function () {
+
+    if (body.classList.contains(dark_theme_class)) {
+
+
+
+        body.classList.remove(dark_theme_class);
+
+    }
+
+    else {
+
+        body.classList.add(dark_theme_class);
+
+    }
+
+});
